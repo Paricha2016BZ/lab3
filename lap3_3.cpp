061a219c2ec33687651298a17bd77aee8a4a7de8
lab303-18 <lab303-18@labeng.local>
2019-01-11 17:36:21 +0700

    3

---
+++ lap3_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   float x=2;
+   float sum=0;
+   while(x<=69){
+       sum=sum+1/x;
+       x=x+1;
+   }
+   cout << sum;
+    return 0;
+}
+ 
